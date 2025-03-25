791f7a9eac2db5a5e57037db19269a5950e762dd
ivi <ivngnz17@gmail.com>
2025-03-25 16:23:35 +0100

    f1

---
+++ pr5_lab/pr5_lab.c
@@ -0,0 +1,13 @@
+/*---------------------------------------------------------------------
+**
+**  Fichero:
+**    pr5_lab.c  15/01/2025
+**
+**    (c) J.M. Mendias
+**    Fundamentos de Computadores II
+**    Facultad de Informática. Universidad Complutense de Madrid
+**
+**  Propósito:
+**    Fichero de código para la práctica 5_lab
+**
+**-------------------------------------------------------------------*/
